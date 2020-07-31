f4cf0b2ef374a8d39b08204f6a556ad0b84ef3ef
praweenaka Ayeshmantha <praweenakahemachandra@gmail.com>
2020-07-31 21:31:17 +0530

    up

--- user_permission.php
+++ user_permission.php
@@ -3,7 +3,7 @@ require_once("./connection_sql.php");
 ?>	
 <!-- Main content -->
 <section class="content">
-
+<!-- sessiom -->
     <div class="box box-primary">
         <div class="box-header with-border">
             <h3 class="box-title">Assign Privileges</h3>
